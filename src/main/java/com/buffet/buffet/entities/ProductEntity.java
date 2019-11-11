daf848883534f931230172fa27d7a85214f6962c
oszkocsilk <oszkocsilk@gmail.com>
2019-11-11 12:02:11 +0100

    home,css refresh

---
+++ src/main/java/com/buffet/buffet/entities/ProductEntity.java
@@ -0,0 +1,51 @@
+package com.buffet.buffet.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "termek")
+public class ProductEntity  {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "termek_id")
+    private int id;
+    @Column(name = "nev")
+    private String name;
+    @Column(name = "ar")
+    private int price;
+    @Column(name = "kategoria")
+    private int category;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getCategory() {
+        return category;
+    }
+
+    public void setCategory(int category) {
+        this.category = category;
+    }
+}
