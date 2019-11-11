fd22771e6387c724456f6ee56ffc517117a15ffc
oszkocsilk <oszkocsilk@gmail.com>
2019-11-11 17:06:46 +0100

    refresh

---
+++ rft-master/src/main/java/com/buffet/buffet/entities/BuffetEntity.java
@@ -0,0 +1,61 @@
+package com.buffet.buffet.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "bufe")
+public class BuffetEntity {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name = "bufe_id")
+    private long id;
+    @Column(name = "nev")
+    private String name;
+    @Column(name = "cim")
+    private String address;
+    @Column(name = "csapat")
+    private int team;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getTeam() {
+        return team;
+    }
+
+    public void setTeam(int team) {
+        this.team = team;
+    }
+
+    @Override
+    public String toString() {
+        return "BuffetEntity{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", team=" + team +
+                '}';
+    }
+}
